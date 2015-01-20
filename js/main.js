942ac6b656eed510e40f5d4453511ba2dd3eb356
Matthew Clark <matthew.clark@undp.org>
2015-01-20 15:57:06 +1100

    First commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Avoid `console` errors in browsers that lack a console.
+;(function($) {
+    $('.product-benefits .col').matchHeight();
+})(jQuery);
+
+// Place any jQuery/helper plugins in here.
+
